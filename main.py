a8280fa7046c37ed4540b3df017fd61b7ade3bba
hundun <wwj.hddjj@gmail.com>
2023-09-09 21:34:25 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pycantonese
 from flask import Flask, jsonify, request
+from pycantonese.word_segmentation import Segmenter
 
 app = Flask(__name__)
 
@@ -18,13 +19,24 @@ def characters_to_jyutping():
 def jyutping_to_yale():
     body = request.get_json()
     text = body['text']
-    jyutping = pycantonese.characters_to_jyutping(text)
-    yale = []
-    for it in jyutping:
-        yale.append([
+    disallow = set(body['disallow']) if ('disallow' in body and body['disallow'] is not None) else set()
+    my_segmenter = Segmenter(disallow=disallow)
+    jyutpings = pycantonese.characters_to_jyutping(text, my_segmenter)
+    yales = []
+    for it in jyutpings:
+        jyutping = it[1]
+        yale = " ".join(pycantonese.jyutping_to_yale(jyutping))
+        print("jyutping = " + (jyutping or 'None') + ", yale = " + (yale or 'None'))
+        yales.append([
             it[0],
-            " ".join(pycantonese.jyutping_to_yale(it[1]))
+            yale
         ])
     return jsonify({
-        'yale': yale
+        'yale': yales
     })
+
+
+if __name__ == '__main__':
+    # corpus = pycantonese.read_chat("data/CHCC.zip")
+    # print("read_chat corpus len = " + str(len(corpus.words())))
+    app.run(debug=False, port=8002)
